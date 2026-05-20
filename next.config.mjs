450b475fb3b1804a65210a057909bd8226a2df74
Arxhent85 <arxhent85@gmail.com>
2026-05-20 15:18:07 +0200

    Configure next.config.mjs for static HTML export

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   // Ensure @google/model-viewer is transpiled for SSR compatibility
